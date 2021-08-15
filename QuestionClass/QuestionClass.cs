cd4e0889dd01541c1fbafe0ab09930be3896b32c
Susitha Bandara <susithapb@gmail.com>
2021-08-15 21:52:14 +0800

    Question 7 - Implementing list iteration.

---
+++ QuestionClass/QuestionClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionClass
+{
+    public static class QuestionClass
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> NamesList = new List<string> { "Jimmy","Jeffrey","John"};
+            NamesList.ForEach((name) => Console.WriteLine(name));
+            Console.ReadLine();
+
+        }
+    }
+}
