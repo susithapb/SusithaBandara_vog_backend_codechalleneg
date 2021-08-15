89cec87781c3684cfc61467d56d9b6c722af63de
Susitha Bandara <susithapb@gmail.com>
2021-08-15 14:38:47 +0800

    Created entities

---
+++ VogCodeChallenge.API/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Entities
+{
+    public class Department : BaseEntity
+    {
+        public string DepartmentName { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ VogCodeChallenge.API/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string JobTitle { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
